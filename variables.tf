fafc75d42ff9f5ca00606bec3da666c047721d1c
Justin <justindelabarra@gmail.com>
2020-12-03 21:34:15 -0500

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "name_rg" {
+  type = string
+  default = "landing-rg-dev"
+}
+
+variable "app_name_res" {
+  type = string
+  default = "landing-app-dev"
+}
+
+variable "db_name_res" {
+  type = string
+  default = "landing-db-dev"
+}
